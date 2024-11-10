596dd1f4f4f64ad67aab6b0935dad6c20407134c
reinatt <reinatthanselwijaya@gmail.com>
2024-11-10 03:46:53 +0100

    finish main.cpp

--- template_cpp/src/src/main.cpp
+++ template_cpp/src/src/main.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <string>
 #include <map>
+#include <unordered_map>
 
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -37,12 +38,24 @@ static void stop(int) {
   exit(0);
 }
 
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator () (const std::pair<T1,T2> &p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+
+        // Mainly for demonstration purposes, i.e. works but is overly simple
+        // In the real world, use sth. like boost.hash_combine
+        return h1 ^ h2;  
+    }
+};
+
+using TheHostMap = std::pair<std::string, unsigned short>;
+using TheMessageMap = std::pair<unsigned short, unsigned long>;
+
 int main(int argc, char **argv) {
   signal(SIGTERM, stop);
   signal(SIGINT, stop);
-
-  const int BATCH_SIZE = 8;
-  const int MSG_PER_SEND = 8;
 
   // `true` means that a config file is required.
   // Call with `false` if no config file is necessary.
@@ -51,42 +64,22 @@ int main(int argc, char **argv) {
   Parser parser(argc, argv);
   parser.parse();
 
-  hello();
-  cout << endl;
-
   std::ifstream configFile(parser.configPath());
   outputFile.open(parser.outputPath());
   unsigned long numberOfMessagesSenderNeedToSend = 0, receiverIdx = 0; 
-  std::map<std::pair<std::string, unsigned short>, unsigned long> hostMap;
-  std::map<std::pair<unsigned short, unsigned long>, bool> messageMap;
+  std::unordered_map<TheHostMap, unsigned long, pair_hash> hostMap;
+  std::unordered_map<TheMessageMap, bool, pair_hash> messageMap;
 
   if (configFile.is_open()) {
     configFile >> numberOfMessagesSenderNeedToSend >> receiverIdx;
     configFile.close();
   }
 
-  cout << "Number of messages sender need to send: " << numberOfMessagesSenderNeedToSend << endl;
-  cout << "Receiver index: " << receiverIdx << endl;
-
-  cout << "My PID: " << getpid() << "\n";
-  cout << "From a new terminal type `kill -SIGINT " << getpid() << "` or `kill -SIGTERM "
-            << getpid() << "` to stop processing packets\n\n";
-
-  cout << "My ID: " << parser.id() << "\n\n";
-
-  cout << "List of resolved hosts is:\n";
-  cout << "==========================\n";
   auto hosts = parser.hosts();
   std::string init_ip = "127.0.0.1";
   auto process_host = Parser::Host(0UL, init_ip, static_cast<unsigned short>(0));
   auto receiver_host = Parser::Host(0UL, init_ip, static_cast<unsigned short>(0));
   for (auto &host : hosts) {
-    cout << host.id << "\n";
-    cout << "Human-readable IP: " << host.ipReadable() << "\n";
-    cout << "Machine-readable IP: " << host.ip << "\n";
-    cout << "Human-readbale Port: " << host.portReadable() << "\n";
-    cout << "Machine-readbale Port: " << host.port << "\n";
-    cout << "\n";
 
     std::pair<std::string, unsigned int> hostKey(host.ipReadable(), host.portReadable());
     hostMap[hostKey] = host.id;
@@ -99,20 +92,6 @@ int main(int argc, char **argv) {
       receiver_host = host;
     }
   }
-  cout << process_host.id << "\n";
-  cout << "\n";
-
-  cout << "Path to output:\n";
-  cout << "===============\n";
-  cout << parser.outputPath() << "\n\n";
-
-  cout << "Path to config:\n";
-  cout << "===============\n";
-  cout << parser.configPath() << "\n\n";
-
-  cout << "Doing some initialization...\n\n";
-
-  cout << "Broadcasting and delivering messages...\n\n";
 
   //input finished, set up UDP 
 
@@ -149,11 +128,9 @@ int main(int argc, char **argv) {
     fd_set socks;
     std::string message = "";
     unsigned long counter = 1;
-    unsigned long current_batch = 1;
     for(unsigned long i = 1; i <= numberOfMessagesSenderNeedToSend; i++){
-      //assuming we have an array of messages, the abstraction is still in the string
       message += std::to_string(i) + "\n";
-      if (i % MSG_PER_SEND == 0 || i == numberOfMessagesSenderNeedToSend){
+      if (i % 8 == 0 || i == numberOfMessagesSenderNeedToSend){
         std::string outputMessage = "";
         for(unsigned int i = 0; i < message.length(); i ++){
           std::string tempString = "";
@@ -164,72 +141,34 @@ int main(int argc, char **argv) {
         outputFile << outputMessage;
         std::string message_to_send = std::to_string(counter) + " " + message;
         counter++;
-        sendto(sockfd, message_to_send.c_str(), message_to_send.length(), 0, reinterpret_cast<const sockaddr*>(&receiver_sa), sizeof(receiver_sa));
-        // while(true){
+        sendto(sockfd, message_to_send.c_str(), message_to_send.size(), 0, reinterpret_cast<const sockaddr*>(&receiver_sa), sizeof(receiver_sa));
+        while(true){
 
-        //   struct timeval t;
-        //   FD_ZERO(&socks);
-        //   FD_SET(sockfd, &socks);
-        //   t.tv_sec = 1;
-        //   t.tv_usec = 0;
-
-        //   int select_result = select(sockfd + 1, &socks, NULL, NULL, &t);
-        //   if(select_result == 0){
-        //     sendto(sockfd, message_to_send.c_str(), message_to_send.size(), 0, reinterpret_cast<const sockaddr*>(&receiver_sa), sizeof(receiver_sa));
-        //   }else if(select_result < 0){
-        //     perror("select failed");
-        //     break;
-        //   }else{
-        //     break;
-        //   }
-        // }
-
-        // n = recvfrom(sockfd, reinterpret_cast<char*>(buffer), 1024, MSG_WAITALL, reinterpret_cast<sockaddr*>(&sender_sa), &len);
-        // buffer[n] = '\0';
-
-        if(i % (BATCH_SIZE*MSG_PER_SEND) == 0 || i == numberOfMessagesSenderNeedToSend){
-          unsigned long highest_ack = 0;
           struct timeval t;
           FD_ZERO(&socks);
           FD_SET(sockfd, &socks);
           t.tv_sec = 1;
           t.tv_usec = 0;
-          while(true){
-            int select_result = select(sockfd + 1, &socks, NULL, NULL, &t);
-            if(select_result == 0){
-              //since the abstraction provides the message string in array, this is still doable
-              std::string batch_fixing_message = "";
-              for(unsigned long j = highest_ack*MSG_PER_SEND+1; j <= current_batch*MSG_PER_SEND*BATCH_SIZE && j <= numberOfMessagesSenderNeedToSend; j ++){
-                batch_fixing_message += std::to_string(j) + "\n";
-                if(j % MSG_PER_SEND == 0 || j == numberOfMessagesSenderNeedToSend){
-                  unsigned long the_message_id = j/MSG_PER_SEND;
-                  std::string outputMessage = std::to_string(the_message_id) + " " + batch_fixing_message;
-                  sendto(sockfd, batch_fixing_message.c_str(), batch_fixing_message.length(), 0, reinterpret_cast<const sockaddr*>(&receiver_sa), sizeof(receiver_sa));
-                  batch_fixing_message = "";
-                }
-              }
-            }else if(select_result < 0){
-              perror("select failed");
-              break;
-            }else{
-              n = recvfrom(sockfd, reinterpret_cast<char*>(buffer), 1024, MSG_WAITALL, reinterpret_cast<sockaddr*>(&sender_sa), &len);
-              buffer[n] = '\0';
-              highest_ack = std::stoul(buffer);
-              if(highest_ack >= current_batch*BATCH_SIZE || highest_ack*8 >= numberOfMessagesSenderNeedToSend){
-                break;
-              }
-            }
+
+          int select_result = select(sockfd + 1, &socks, NULL, NULL, &t);
+          if(select_result == 0){
+            sendto(sockfd, message_to_send.c_str(), message_to_send.size(), 0, reinterpret_cast<const sockaddr*>(&receiver_sa), sizeof(receiver_sa));
+          }else if(select_result < 0){
+            perror("select failed");
+            break;
+          }else{
+            break;
           }
-          current_batch++;
         }
+
+        n = recvfrom(sockfd, reinterpret_cast<char*>(buffer), 1024, MSG_WAITALL, reinterpret_cast<sockaddr*>(&sender_sa), &len);
+        buffer[n] = '\0';
 
         message = "";
       }
     }
 
   }else{
-    unsigned long processes_highest_ack[200];
-    memset(processes_highest_ack, 0, sizeof(processes_highest_ack));
     //receiver
     while (true) {
       //wait to receive and deliver the messages
@@ -250,10 +189,7 @@ int main(int argc, char **argv) {
         }
         unsigned long message_id = std::stoul(message_id_str);
         if(messageMap.find(std::make_pair(it->second, message_id)) != messageMap.end()){
-          if(message_id % BATCH_SIZE == 0){
-            std::string send_buffer = std::to_string(processes_highest_ack[it->second]);
-            sendto(sockfd, send_buffer.c_str(), send_buffer.length(), 0, reinterpret_cast<const sockaddr*>(&sender_sa), len);
-          }
+          sendto(sockfd, "0", sizeof("0"), 0, reinterpret_cast<const sockaddr*>(&sender_sa), len);
           continue;
         }
 
@@ -267,20 +203,11 @@ int main(int argc, char **argv) {
         }
         messageMap[std::make_pair(it->second, message_id)] = true;
         outputFile << message_to_deliver;
-        if(processes_highest_ack[it->second]+1 == message_id){
-          unsigned long cur_message_id = message_id+1;
-          while(messageMap.find(std::make_pair(it->second, cur_message_id)) != messageMap.end()){
-            cur_message_id++;
-          }
-          processes_highest_ack[it->second] = cur_message_id-1;
-        }
-        if(message_id % BATCH_SIZE == 0){
-          std::string send_buffer = std::to_string(processes_highest_ack[it->second]);
-          sendto(sockfd, send_buffer.c_str(), send_buffer.length(), 0, reinterpret_cast<const sockaddr*>(&sender_sa), len);
-        }
+
       }else{
         perror("host not found");
       }
+      sendto(sockfd, "0", sizeof("0"), 0, reinterpret_cast<const sockaddr*>(&sender_sa), len);
       // cout << inet_ntoa(sender_sa.sin_addr) << ' ' << sender_sa.sin_port << endl;
     }
   }
